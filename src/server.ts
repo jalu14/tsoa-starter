49382a3614136760a8fbb168340b7a47c9848693
jalu <javidevictoria@gmail.com>
2021-10-15 11:38:05 +0200

    Commit

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { app } from "./app";
 const port = process.env.PORT || 8080;
 
 app.listen(port, () =>
-    console.log(`Renfe api listening at port: ${port}`)
+    console.log(`App listening on port: ${port}`)
 );
